15bf85ca7f45057847bccd530c1a962d73618255
victor perez <victor.perez.martos@est.fib.upc.edu>
2016-01-11 18:34:57 +0100

    obte representacions

--- src/main/java/com/shows/as/domain/classes/Espectacle.java
+++ src/main/java/com/shows/as/domain/classes/Espectacle.java
@@ -1,15 +1,25 @@
 package com.shows.as.domain.classes;
 
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Espectacle {
 
     private String titol;
     private Integer participants;
-    private Representacio representacio;
+    private Set<Representacio> representacions;
 
     public String getTitol() {
         return titol;
     }
 
-    // TODO obteRepresentacions
-
+    // TODO obteRepresentacions --> result ha de ser un set de tuples --> r.getInfo();
+    public Set<Representacio> obteRepresentacions(Date data){
+        Set<Representacio> result = new LinkedHashSet<Representacio>();
+        for(Representacio  r: representacions){
+            if(data == r.getDate())result.add(r);
+        }
+        return result;
+    }
 }
--- src/main/java/com/shows/as/domain/useCases/ConsultarRepresentacions.java
+++ src/main/java/com/shows/as/domain/useCases/ConsultarRepresentacions.java
@@ -1,6 +1,9 @@
 package com.shows.as.domain.useCases;
 
+import com.shows.as.domain.classes.Espectacle;
 import com.shows.as.domain.classes.Representacio;
+import com.shows.as.domain.controllers.CtrlEspectacle;
+import com.shows.as.domain.factories.FactoriaCtrl;
 
 import java.util.Date;
 import java.util.LinkedHashSet;
@@ -9,18 +12,24 @@ import java.util.Set;
 public class ConsultarRepresentacions {
 
     public Set<String> consultaEspectacles() {
+        FactoriaCtrl factoriaCtrl = FactoriaCtrl.getInstance();
+        CtrlEspectacle ctrlEspectacle = factoriaCtrl.getCtrlEspectacle();
+        Set<Espectacle> espectacles = ctrlEspectacle.getAll();
+        if(espectacles.size() == 0)throw new IllegalStateException("No hi ha espectacles");
         Set<String> result = new LinkedHashSet<String>();
-
-
-
+        for(Espectacle e : espectacles){
+            result.add(e.getTitol());
+        }
         return result;
     }
 
+    //todo revisar pq segons esquema retorna set de tuples
     public Set<Representacio> consultaRepresentacions(String titol, Date data) {
+        FactoriaCtrl factoriaCtrl = FactoriaCtrl.getInstance();
+        CtrlEspectacle ctrlEspectacle = factoriaCtrl.getCtrlEspectacle();
+        Espectacle espectacle = ctrlEspectacle.getEspectacle(titol);
         Set<Representacio> result = new LinkedHashSet<Representacio>();
-
-
-
+        result = espectacle.obteRepresentacions(data);
         return result;
     }
 }
