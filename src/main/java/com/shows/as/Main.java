128356c5afa2de7c78560f2fde217aa90482fb45
Albert Suarez <albert.suarez.molgo@est.fib.upc.edu>
2016-01-14 22:51:40 +0100

    jocs de prova 1!!

--- src/main/java/com/shows/as/Main.java
+++ src/main/java/com/shows/as/Main.java
@@ -1,12 +1,6 @@
 package com.shows.as;
 
-import com.shows.as.domain.classes.*;
-import com.shows.as.domain.enums.TipusSessio;
-import com.shows.as.domain.utils.Utils;
-import com.shows.as.persistence.HibernateUtils;
 import com.shows.as.presentation.controllers.ComprarEntradesController;
-
-import java.sql.Date;
 
 public class Main {
 
@@ -18,18 +12,18 @@ public class Main {
 
     private static void carregaBaseDadesCas1() {
 
-        //HibernateUtils.save(new Espectacle("Cats", 10));
-        //HibernateUtils.save(new Espectacle("El Rey Leon", 20));
-        //HibernateUtils.save(new Espectacle("La Sirenita", 20));
-        //HibernateUtils.save(new Espectacle("Frozen", 20));
-        //HibernateUtils.save(new Espectacle("La Cantina", 20));
-        //HibernateUtils.save(new Sessió(TipusSessio.mati.toString()));
-        //HibernateUtils.save(new Sessió(TipusSessio.tarda.toString()));
-        //HibernateUtils.save(new Sessió(TipusSessio.nit.toString()));
-        //HibernateUtils.save(new Local("Cyrano", "Aribau 21"));
-        //HibernateUtils.save(new Local("Bling Bling", "Tusset 49"));
-        //HibernateUtils.save(new Local("Apolo", "Paralel 845"));
-        /*for (int i = 0; i < 4; i++) {
+        /*HibernateUtils.save(new Espectacle("Cats", 10));
+        HibernateUtils.save(new Espectacle("El Rey Leon", 20));
+        HibernateUtils.save(new Espectacle("La Sirenita", 20));
+        HibernateUtils.save(new Espectacle("Frozen", 20));
+        HibernateUtils.save(new Espectacle("La Cantina", 20));
+        HibernateUtils.save(new Sessió(TipusSessio.mati.toString()));
+        HibernateUtils.save(new Sessió(TipusSessio.tarda.toString()));
+        HibernateUtils.save(new Sessió(TipusSessio.nit.toString()));
+        HibernateUtils.save(new Local("Cyrano", "Aribau 21"));
+        HibernateUtils.save(new Local("Bling Bling", "Tusset 49"));
+        HibernateUtils.save(new Local("Apolo", "Paralel 845"));
+        for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 8; j++) {
                 HibernateUtils.save(new Seient(i, j, "Cyrano"));
             }
@@ -43,8 +37,8 @@ public class Main {
             for (int j = 0; j < 8; j++) {
                 HibernateUtils.save(new Seient(i, j, "Apolo"));
             }
-        }*/
-        /*HibernateUtils.save(new Representació(50.0, Utils.createDate(14, 1, 2016), 32, "mati", "Cyrano", "Cats"));
+        }
+        HibernateUtils.save(new Representació(50.0, Utils.createDate(14, 1, 2016), 32, "mati", "Cyrano", "Cats"));
         HibernateUtils.save(new Representació(60.0, Utils.createDate(14, 1, 2016), 32, "nit", "Cyrano", "El Rey Leon"));
         HibernateUtils.save(new Representació(70.0, Utils.createDate(15, 1, 2016), 32, "tarda", "Cyrano", "Frozen"));
         HibernateUtils.save(new Representació(40.0, Utils.createDate(14, 1, 2016), 32, "mati", "Bling Bling", "Cats"));
@@ -52,9 +46,12 @@ public class Main {
         HibernateUtils.save(new Representació(20.0, Utils.createDate(15, 1, 2016), 32, "tarda", "Bling Bling", "Frozen"));
         HibernateUtils.save(new Representació(10.0, Utils.createDate(14, 1, 2016), 32, "mati", "Apolo", "Cats"));
         HibernateUtils.save(new Representació(70.0, Utils.createDate(14, 1, 2016), 32, "nit", "Apolo", "El Rey Leon"));
-        HibernateUtils.save(new Representació(50.0, Utils.createDate(15, 1, 2016), 32, "tarda", "Apolo", "La Sirenita"));*/
+        HibernateUtils.save(new Representació(50.0, Utils.createDate(15, 1, 2016), 32, "tarda", "Apolo", "La Sirenita"));
 
-        //HibernateUtils.save(new Estrena(50.0, Utils.createDate(14, 1, 2016), 32, "mati", "Cyrano", "Cats",1));
+        HibernateUtils.save(new Estrena(60.0, Utils.createDate(14, 1, 2016), 32, "nit", "Cyrano", "El Rey Leon",1));
+        HibernateUtils.save(new Estrena(70.0, Utils.createDate(14, 1, 2016), 32, "nit", "Apolo", "El Rey Leon",1));
+        HibernateUtils.save(new Estrena(30.0, Utils.createDate(14, 1, 2016), 32, "nit", "Bling Bling", "La Cantina",1));
+
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 8; j++) {
@@ -100,7 +97,6 @@ public class Main {
             for (int j = 0; j < 8; j++) {
                 HibernateUtils.save(new Seientenrepresentació("lliure",i, j, "Bling Bling","tarda",null));
             }
-        }
-        //HibernateUtils.save(new Entrada(1,);
+        }*/
     }
 }
